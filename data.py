1260fc077c4c45889ee04b90302c6ce2363a77a5
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-01-26 12:48:04 +0300

    data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+9headers = {
+    "Content-Type": "application/json",
+    }
+
+user_body = {
+    "firstName": "Валерия",
+    "phone": "+79779812136",
+    "address": "г. Москва, ул. Тихомирова, д. 19"
+}
+
+kit_body = {
+    "name": "Мой набор"
+}
